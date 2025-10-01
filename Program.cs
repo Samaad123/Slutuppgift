3b4c7ceeac14b0ded04c56d3440142438ca079af
github username <github email>
2025-10-01 12:02:11 +0200

    User login created
    becomes an active user

--- Program.cs
+++ Program.cs
@@ -2,20 +2,25 @@
 // 
 
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 using Slutuppgift;
 
 using Slutuppgift;
         // Lägger till lista på de enkla saker som behövs medans programmet kör
         List<User> users = new List<User>();
         List<Item> items = new List<Item>();
-        //List<TradeRequest> TradeRequests = new();
-        //List<TradeRequest> CompletedTrades = new();
+//List<TradeRequest> TradeRequests = new();
+//List<TradeRequest> CompletedTrades = new();
 
+User? active_user = null;
 
+User user_ett = new User ("Lukas", "Lösenord");
+Item ettItem = new Item("Dator", "Svart", user_ett);
 
-
+Console.WriteLine("användare ett har lösenord: " + ettItem.Owner._password);
         bool running = true;
 
 while (running)
@@ -23,14 +28,14 @@ while (running)
     Console.WriteLine("Välj ett alternative:");
     Console.WriteLine("1. Registrera Konto");
     Console.WriteLine("2. Logga in");
-    Console.WriteLine("3. Logga ut");
-    Console.WriteLine("4. Ladda upp item");
-    Console.WriteLine("5. List Others Items");
-    Console.WriteLine("6. Begär en trade");
-    Console.WriteLine("7. Acceptera Request");
-    Console.WriteLine("8. Neka trade");
-    Console.WriteLine("9. Visa genomförda trades");
-    Console.WriteLine("0. Avsluta");
+    // Console.WriteLine("3. Logga ut");
+    // Console.WriteLine("4. Ladda upp item");
+    // Console.WriteLine("5. List Others Items");
+    // Console.WriteLine("6. Begär en trade");
+    // Console.WriteLine("7. Acceptera Request");
+    // Console.WriteLine("8. Neka trade");
+    // Console.WriteLine("9. Visa genomförda trades");
+    // Console.WriteLine("0. Avsluta");
 
     string choice = Console.ReadLine(); // skapar en string till det som usern väljer
 
@@ -41,7 +46,7 @@ while (running)
             break;
 
         case "2":
-            //Login();
+            Login();
             break;
 
         case "3":
@@ -94,7 +99,10 @@ while (running)
 
 
 
-// methods
+
+
+
+
 
 void Register()
 {
@@ -108,8 +116,31 @@ void Register()
     users.Add(newUser);
 
     Console.WriteLine("New account added successfully");
+    
 }
 
+void Login()
+{
+    Console.Write("Write your email: ");
+    string userEmail = Console.ReadLine();
+    Console.Write("Write your password: ");
+    string password = Console.ReadLine();
+
+foreach (User test in users)
+{
+    if (userEmail == test.Email && password == test._password)
+    {
+        active_user = test;
+    }
+    else
+    {
+        Console.WriteLine("Fel info");
+        }
+    }
+Console.WriteLine("inloggad användare är: " + active_user.Email);
+
+
+}
 
 
 //while (bool = running );
